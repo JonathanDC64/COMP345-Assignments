72820911e520742014e4bd2aaab3b12f09e77d67
justinbaron1 <justinbaron1@hotmail.com>
2017-10-16 20:26:34 -0400

    header files doxygen

--- Assignment2/Assignment2/query_result.h
+++ Assignment2/Assignment2/query_result.h
@@ -3,12 +3,17 @@
 
 class query_result
 {
+	/*! prints the name of the document with its score */
 	friend std::ostream& operator<<(std::ostream& os, const query_result & qr);
 public:
+
+	/*! constructs a query_result using a document and its score */
 	query_result(const document & found_document, const double score);
+
+	/*! Destructor */
 	~query_result();
 
-	document found_document;
-	double score;
+	document found_document; // document
+	double score; // score of the document
 	
 };
--- Assignment2/Assignment2/stopword.h
+++ Assignment2/Assignment2/stopword.h
@@ -5,14 +5,25 @@
 
 class stopword
 {
+	/*! prints the number of stop words */
 	friend std::ostream& operator<<(std::ostream& os, const stopword & sw);
 public:
+
+	/*! Default constructor */
 	stopword();
+
+	/*! Constructs a stopword object using the name of a file */
 	stopword(const std::string & file_name);
+
+	/*! Destructor */
 	~stopword();
+
+	/*! Returns true if the given word is a stopword, else false */
 	bool exists(const std::string & word) const;
+
+	/*! allows exists() method to be called like the following: stopwords(word) */
 	bool operator()(const std::string & word) const;
 private:
-	std::map<std::string, std::string> stopwords;
+	std::map<std::string, std::string> stopwords; // map containing all the stopwords
 };
 
